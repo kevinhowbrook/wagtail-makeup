80d7f34e0f1b95ef05d45b20d47461e4d2d7794d
Kevin Howbrook <kbhowbrook@gmail.com>
2021-01-21 22:50:16 +0000

    Test settings

--- wagtailmakeup/tests/tests.py
+++ wagtailmakeup/tests/tests.py
@@ -1,8 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from ..settings import WagtailMakeUpSettings
 
 class MakeupTest(TestCase):
-    def test_math(self):
-        self.assertEqual(1,1)
+
+    def test_settings_missing(self):
+        wagtail_makeup_settings = WagtailMakeUpSettings()
+        self.assertEqual(wagtail_makeup_settings.CLIENT_ID, None)
+        self.assertEqual(wagtail_makeup_settings.CLIENT_SECRET, None)
+
+    @override_settings(WAGTAIL_UNSPLASH={'CLIENT_ID': 1234,'CLIENT_SECRET': 'shh'})
+    def test_settings_defined(self):
+        settings = WagtailMakeUpSettings()
+        self.assertEqual(settings.CLIENT_ID, 1234)
+        self.assertEqual(settings.CLIENT_SECRET, 'shh')
 
     # Test settings are defined, if not show a warning
 
